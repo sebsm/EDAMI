295f18d3cf4241ffcd35ae64d6cfaea5a3f99230
selvert <01102827@pw.edu.pl>
2021-01-06 23:49:03 +0100

    Small changes

--- TASK4_EDAMI_SMOLINSKI.R
+++ TASK4_EDAMI_SMOLINSKI.R
@@ -24,7 +24,7 @@ download.file('http://archive.ics.uci.edu/ml/machine-learning-databases/wine-qua
 
 red_wine = read.table("wine_red.csv", header = TRUE, sep=";", na.strings= "*")
 
-red_wine
+#red_wine
 
 # Some information about dataset
 
@@ -32,10 +32,32 @@ summary(red_wine)
 View(red_wine)
 set.seed(2137)
 
-# Creating training and test datasets 
+
+# Creating a new attribute with 3 values(classses) based on 
+# the orignal class atribute - quality
+
+red_wine$new = lapply(red_wine[,12], function (x)
+{
+  if(x >6)  { "A"}
+  else if(x >4)  {"B"}
+  else { "C"} 
+}
+)
+red_wine$new = unlist(red_wine$new)
+red_wine$new = as.factor(red_wine$new)
+
+# Show how does the table with content looks like
+View(red_wine)
+
+?unlist
+?as.factor
+
+# Basic check of our dataset's parameters and their distribution #
+
+# Creating data partitions (test and training) for our basic dataset and the quality attribute
 nrow(red_wine)
 ?sample
-sam <- sample(3, nrow(red_wine), replace=TRUE, prob=c(0.7, 0.3))
+sam <- sample(2, nrow(red_wine), replace=TRUE, prob=c(0.7, 0.3))
 sam
 red_wine_train <- red_wine[sam==1,]
 red_wine_test <- red_wine[sam==2,]
@@ -44,18 +66,35 @@ red_wine_test <- red_wine[sam==2,]
 prop.table(table(red_wine_train$quality))
 prop.table(table(red_wine_test$quality))
 
-# Creating training and test datasets 
+# Creating data partitions (test and training) for our modified dataset and the category attribute 
 ?createDataPartition
-red_wine_quality <- unlist(createDataPartition(red_wine$quality,p=0.7))
-str(red_wine_quality)
+red_wine_category <- unlist(createDataPartition(red_wine$new,p=0.7))
+str(red_wine_category)
 
-red_wine_train_qual <-red_wine[red_wine_quality,]
-red_wine_test_qual <-red_wine[-red_wine_quality,]
+red_wine_train_cat <-red_wine[red_wine_category,]
+red_wine_test_cat <-red_wine[-red_wine_category,]
 
 # Class distribution in sets
-prop.table(table(red_wine_train_qual$category))
-prop.table(table(red_wine_test_qual$category))
+prop.table(table(red_wine_train_cat$new))
+prop.table(table(red_wine_test_cat$new))
 
-table(red_wine_train_qual$category)
-table(red_wine_test_qual$category)
+table(red_wine_train_qual$new)
+table(red_wine_test_qual$new)
+
+
+##################################################
+# Experiments with C5.0 classifier               #
+##################################################
+
+
+
+
+
+
+
+
+
+
+
+
 
